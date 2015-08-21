8930f7b22c128a9ad836e4e829add6bcaa9120f0
joakorelhelou <j.rodriguez@globant.com>
2015-08-21 10:38:14 -0300

    Class 6 : Build form

--- students.js
+++ students.js
@@ -1,4 +1,5 @@
 var studentsBookApp = angular.module("studentsBookApp", []);
+
 studentsBookApp.controller("StudentsController", function StudentsController($scope){ //Define Controller
 	$scope.students=[ // Create Student Model with three attributes
 		{name:"John White", phone: "1234579", class: "2° ESO"},
@@ -6,4 +7,10 @@ studentsBookApp.controller("StudentsController", function StudentsController($sc
 		{name: "Albert Smith", phone: "5646546", class: "2° ESO"},
 		{name: "Ana Marine", phone: "46549879", class: "3° ESO"}
 	];
+
+
+	$scope.Save=function(){
+		$scope.students.push({name:$scope.newStudent.name, phone:$scope.newStudent.phone, class:$scope.newStudent.class});
+	}
+
 });
