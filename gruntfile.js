8a34f28683505aee537d043418cab726dddf51c9
joakorelhelou <j.rodriguez@globant.com>
2015-08-24 09:34:18 -0300

    Class 11 - Concat and uglify js files with Grunt

--- gruntfile.js
+++ gruntfile.js
@@ -2,8 +2,23 @@ module.exports = function(grunt){
 	grunt.initConfig({
 		jshint:{
 			all:['students.js']
+		},
+		concat:{
+			dist:{
+				src:['students.js', 'script1.js', 'script2.js'],
+				dest: 'united.js'
+			}
+		},
+		uglify:{
+			dist:{
+				src: 'united.js',
+				dest: 'build/united.min.js'
+			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default',['jshint']);
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('default',['jshint', 'concat', 'uglify']);
 };
